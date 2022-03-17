0cd1f6af0d0a9307fe75480fd0ca54cb6fbf2dc3
Poggecci <alejandro.cruzado@upr.edu>
2022-03-16 23:35:39 -0400

    Added Auth0

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
+import LoginButton from './components/login/LoginButton';
+import LogoutButton from './components/logout/LogoutButton';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { isAuthenticated } = useAuth0();
+    return (
+        <div className="App">
+            <header className="App-header">
+                <LoginButton />
+                <LogoutButton />
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/logout/LogoutButton.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+function LogoutButton() {
+    const { logout, isAuthenticated } = useAuth0();
+    if (isAuthenticated){
+        return (
+            <button onClick={()=>logout()} >
+                Log Out
+            </button>
+        )
+    } else{
+        return <div></div>
+    }
+}
+
+export default LogoutButton
