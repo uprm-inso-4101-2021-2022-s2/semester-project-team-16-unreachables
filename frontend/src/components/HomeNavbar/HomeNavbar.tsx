aa0858b686293e926ffe7cd448ab8702b60edc22
Poggecci <alejandro.cruzado@upr.edu>
2022-04-07 10:58:48 -0400

    Added Outage Report Interface, Updated Client Reports, and Updated Navbar

--- frontend/src/components/HomeNavbar/HomeNavbar.tsx
+++ frontend/src/components/HomeNavbar/HomeNavbar.tsx
@@ -3,31 +3,36 @@ import Container from 'react-bootstrap/esm/Container'
 import Nav from 'react-bootstrap/esm/Nav'
 import Navbar from 'react-bootstrap/esm/Navbar'
 import NavDropdown from 'react-bootstrap/esm/NavDropdown'
-import LoginButton from '../login/LoginButton'
-import LogoutButton from '../logout/LogoutButton'
+import LoginButton from '../LoginButton/LoginButton'
+import LogoutButton from '../LogoutButton/LogoutButton'
 import './HomeNavbar.scss'
 function HomeNavbar() {
-  return (
-    <Navbar bg="dark" expand="lg">
-    <Container>
-        <Navbar.Brand href="#home">Outage Tracker</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="btn-light"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-            <LoginButton />
-            <LogoutButton />
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-        </Nav>
-        </Navbar.Collapse>
-    </Container>
-    </Navbar>
-  )
+    const isEmployee = false; //placeholder for when we do employee authentication
+    let title;
+    if (isEmployee){
+        title = <Navbar.Brand href="#home">Outage Tracker - Employee</Navbar.Brand>
+    } else{
+        title = <Navbar.Brand href="#home">Outage Tracker</Navbar.Brand>
+    }
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+            {title}
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className="btn-light"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <LoginButton />
+                <LogoutButton />
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#map">Outage Map</NavDropdown.Item>
+                    <NavDropdown.Item href="#table">Outage Table</NavDropdown.Item>
+                    <NavDropdown.Item href="#contact ">Contact Us</NavDropdown.Item>   
+                </NavDropdown>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    )
 }
 
-export default HomeNavbar
+    export default HomeNavbar
